afcde457d35dbb9fdd15e9f3080414597abf9ff2
Your Name <you@example.com>
2018-05-12 14:05:12 +0530

    added classification

--- cn.py
+++ cn.py
@@ -1,4 +1,5 @@
 # Importing the Keras libraries and packages
+import keras
 from keras.models import Sequential
 from keras.layers import Conv2D
 from keras.layers import MaxPooling2D
@@ -30,28 +31,39 @@ test_datagen = ImageDataGenerator(rescale = 1./255)
 training_set = train_datagen.flow_from_directory('train_data',
 target_size = (50, 65),
 batch_size = 32,
-class_mode = 'binary')
+class_mode = 'sparse')
 test_set = test_datagen.flow_from_directory('test_data',
 target_size = (50, 65),
 batch_size = 32,
-class_mode = 'binary')
+class_mode = 'sparse')
 classifier.fit_generator(training_set,
 steps_per_epoch = 800,
-epochs = 25,
+epochs = 5,
 validation_data = test_set,
-validation_steps = 2000)
+validation_steps = 20)
 # Part 3 - Making new predictions
-import numpy as np
-from keras.preprocessing import image
-test_image = image.load_img('single_prediction/1.png', target_size = (50, 65))
-test_image = image.img_to_array(test_image)
-test_image = np.expand_dims(test_image, axis = 0)
-result = classifier.predict(test_image)
-training_set.class_indices
+#
+# classifier.save('store')
 
-if result[0][0] == 1:
-    prediction = 'A'
-else:
-    prediction = 'B'
+# classifier = keras.models.load_model('store')
+#
+# import numpy as np
+# from keras.preprocessing import image
+# test_image = image.load_img('single_prediction/228_4.png', target_size = (50, 65))
+# test_image = image.img_to_array(test_image)
+# test_image = np.expand_dims(test_image, axis = 0)
+# result = classifier.predict(test_image)
 
-print(prediction)
+# file = open('class_indices.txt','w')
+# file.write(str(training_set.class_indices))
+# print(training_set.class_indices)
+
+
+# print("results")
+print(result)
+# if result[0][0] == 0:
+#     prediction = 'A'
+# else:
+#     prediction = 'B'
+
+# print(prediction)
