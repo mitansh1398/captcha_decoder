383aa6aac3eddc2a1587df05c5197788e57d7d4c
mitansh1398 <mitansh1398@iitg.ernet.in>
2018-05-17 22:59:53 +0530

    revamping

--- classify_test_data.py
+++ classify_test_data.py
@@ -1,4 +1,4 @@
-#Create test
+# Create test
 import pickle
 import numpy as np
 import shutil
@@ -6,28 +6,27 @@ from keras.preprocessing import image
 import keras
 import os
 
-
+#load pretrained model
 classifier = keras.models.load_model('store')
-pickle_in = open("dict.pickle","rb")
+pickle_in = open("dict.pickle", "rb")
 label = pickle.load(pickle_in)
 
 directory = "output"
 for file in os.listdir(directory):
-	test_image = image.load_img('output/'+file, target_size = (50, 65))
-	test_image = image.img_to_array(test_image)
-	test_image = np.expand_dims(test_image, axis = 0)
-	result = classifier.predict(test_image)
+    test_image = image.load_img('output/' + file, target_size=(50, 65))
+    test_image = image.img_to_array(test_image)
+    test_image = np.expand_dims(test_image, axis=0)
+    result = classifier.predict(test_image)
 
+    y = result > 0.9
+    y = y[0]
+    ans = ""
+    for index, i in enumerate(y):
+        if i == True:
+            ans = str([l for l in label if label[l] == index][0])
 
-	y = result>0.9
-	y = y[0]
-	ans=""
-	for index,i in enumerate(y):
-		if i == True:
-			ans = str([l for l in label if label[l]==index][0])
+    shutil.move("output/" + file, "predictions/" + ans + "/" + file)
 
-	shutil.move("output/"+file, "predictions/"+ans+"/"+file)
+    print("labels : " + str(label))
 
-	print("labels : " + str(label))
-
-	print("prediction of image is : "+ans)
+    print("prediction of image is : " + ans)
